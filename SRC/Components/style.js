bd1003760a236fdfd8cfca958d3d9a3885326e20
najuu <ana.dalolio@etec.sp.gov.br>
2024-10-08 12:10:03 -0300

    branch com o código da aula do dia Tue, Oct  8, 2024 12:10:03 PM

---
+++ SRC/Components/style.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+BoxTitle: {
+alignItems: "center",
+
+},
+texTitle: {
+color: "#EA3829",
+},
+});
+export default styles
